db6b91d43f8e746288a24f87b1f210e3537ceae4
Yash Rathod <yashvrathod15@gmail.com>
2025-11-28 14:46:44 +0530

    third commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg"; // Make sure this is installed
 
 const adapter = new PrismaPg({
